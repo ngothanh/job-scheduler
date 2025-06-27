d023fdc85197504eebaecefefa9e8f36c75669d7
edward.ngo <thanh.ngo.2510@gmail.com>
2025-06-28 00:17:32 +0700

    Finish naive implementation

---
+++ src/main/java/com/submiliseconds/jobscheduler/DefaultJobScheduler.java
@@ -0,0 +1,25 @@
+package com.submiliseconds.jobscheduler;
+
+import java.time.Duration;
+
+public class DefaultJobScheduler implements JobScheduler {
+
+    private JobExecutor executor;
+
+    @Override
+    public void submit(Job job) {
+        executor.execute(job);
+    }
+
+    @Override
+    public void submit(Job job, Duration delay) {
+        new Thread(() -> {
+            try {
+                Thread.sleep(delay.toMillis());
+                executor.execute(job);
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+            }
+        }).start();
+    }
+}
